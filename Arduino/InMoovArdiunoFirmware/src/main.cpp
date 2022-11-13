dfda42435d50e0ec6399eeafe4379a075d30b9e2
Emgimeer-Bazder <ericpazder@hotmail.ca>
2022-11-12 21:27:20 -0800

    Prints name over serial when flashed to uno.

--- Arduino/InMoovArdiunoFirmware/src/main.cpp
+++ Arduino/InMoovArdiunoFirmware/src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 
+//A simple program that will transmit everyones name over serial, used to practicing version control.
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello from Eric Pazder !");
+  Serial.println("Hello from your name !");
 }
