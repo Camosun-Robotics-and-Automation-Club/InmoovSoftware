1105fde68c71624f52bcbf83e1ae95e9716b2633
Sebastien Robitaille <sebastien@robitaille.info>
2022-12-26 19:53:34 -0800

    use gear ratio and min/max PWM for servo movement

--- INMOOV-Head-Control/StandardServo.cpp
+++ INMOOV-Head-Control/StandardServo.cpp
@@ -2,52 +2,112 @@
 #include <Servo.h>
 #include "StandardServo.h"
 
-StandardServo::StandardServo(byte pin) {
-  m_pin = pin;
-}
-StandardServo::StandardServo(byte pin, float PWMPerDegree, int minRotation, int maxRotation , bool reverseDirection, int defaultRotation) {
-  m_pin = pin;
-  m_PWMPerDegree = PWMPerDegree;
-  m_minRotation = ((minRotation * m_PWMPerDegree) + 1000);
-  m_maxRotation = ((maxRotation * m_PWMPerDegree) + 1000);
-  m_reverseDirection = reverseDirection;
-  m_defaultRotation  = constrain(((defaultRotation * m_PWMPerDegree) + 1000), m_minRotation, m_maxRotation);
+StandardServo::
+
+StandardServo::StandardServo(byte pin){
+  m_pin = pin; 
 }
 
-void StandardServo::attach() {
-  m_servo.attach(m_pin);
-  moveToTargetMicroseconds(m_defaultRotation);
-  m_currentRotation = m_defaultRotation;
+StandardServo::StandardServo(byte pin, 
+                            int minPWMTime,
+                            int maxPWMTime,
+                            int degreeRange,                
+                            float outputGearRatio,          
+                            bool reverseDirection,
+                            int minRotation, 
+                            int maxRotation, 
+                            int startingRotation) {
+  m_pin = pin;                                              //Data pin number for the servo
+  m_minPWMTime = minPWMTime;                                //Absolute minimum timing for the servo
+  m_maxPWMTime = maxPWMTime;                                //Aboslute maximum timing for the servo
+  m_degreeRange = degreeRange;                              //The range of rotation for the servo in degrees
+
+  m_outputGearRatio = outputGearRatio;                      //Multiplier for servos conecting to part using a mechanical advantage input matamatical representation eg. 0.5 = 2/1
+  m_reverseDirection = reverseDirection;                    //Is the rotaion of the servo diffrent to the roation of the part
+
+  m_minRotation = mapf((minRotation / m_outputGearRatio),    //PWM time minimum for part to rotate full range
+                      0, 
+                      m_degreeRange, 
+                      m_minPWMTime, 
+                      m_maxPWMTime);
+                      
+  m_maxRotation = mapf((maxRotation / m_outputGearRatio),    //PWM time maximum for part to rotate full range
+                      0, 
+                      m_degreeRange, 
+                      m_minPWMTime, 
+                      m_maxPWMTime);
+
+  m_startingRotation = mapf((startingRotation / m_outputGearRatio),  //PWM time to set when the .attach() function is called for home position
+                            0, m_degreeRange, 
+                            m_minPWMTime, 
+                            m_maxPWMTime);
+
+  m_currentRotation = startingRotation;                     //Current PWM time of the servo
 }
 
-bool StandardServo::moveTowardsTarget(float degreesToMove) {
-  int newRotation = m_currentRotation + (degreesToMove * m_PWMPerDegree);//Get the new roation
-  return moveToTargetMicroseconds(newRotation);
+float StandardServo::mapf(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-bool StandardServo::moveToTarget(int target){
-  target = ((target * m_PWMPerDegree) + 1000);
-  return moveToTargetMicroseconds(target);
-}
-
-bool StandardServo::moveToTargetMicroseconds(int target){
+bool StandardServo::moveToTargetMicroseconds(float target){   
   bool validMove;
-  if((target > m_maxRotation) || (target < m_minRotation)){//Check if it is in bounds
+  if((target > m_maxRotation) || (target < m_minRotation)){ //Check if target rotation is in bounds
     validMove = false;
   }else{
     validMove = true;
   }
-  target = constrain(target, m_minRotation, m_maxRotation);//Constain target roation to bounds
+
+  target = constrain(target, m_minRotation, m_maxRotation); //Constain target roation to bounds
+
   if(m_reverseDirection){
-    m_servo.writeMicroseconds(map(target,1000,2000,2000,1000));//Write target rotation flipped to servo
+    m_servo.writeMicroseconds(int(mapf(target,
+                                  m_minRotation,
+                                  m_maxRotation,
+                                  m_maxRotation,
+                                  m_minRotation)));         //Write target rotation flipped to servo
   }else{
-    m_servo.writeMicroseconds(target);//Write target rotation to servo
+    m_servo.writeMicroseconds(int(target));                 //Write target rotation to servo
   }
-  m_currentRotation = target;//Update current posistion
-  return validMove;//Return if the move was in bounds
+
+  m_currentRotation = target;                               //Update current posistion
+  return validMove;                                         //Return if the move was in bounds
   
 }
 
-float StandardServo::getPWMPerDegree(){return m_PWMPerDegree;}
+void StandardServo::attach() {
+  m_servo.attach(m_pin);                                    //Attach the servo
+  moveToTargetMicroseconds(m_startingRotation);             //Rotate to the starting rotation
+  m_currentRotation = m_startingRotation;                   //Update the current rotation
+}
 
-float StandardServo::getCurrentRotation(){return ((m_currentRotation - 1000)/m_PWMPerDegree);}
+bool StandardServo::moveRelative(float degreesToMove) {
+  int newRotation = m_currentRotation + (mapf((degreesToMove / m_outputGearRatio),
+                                            0, 
+                                            m_degreeRange, 
+                                            m_minPWMTime, 
+                                            m_maxPWMTime)) - m_minPWMTime;  //Remapf the input degrees to servo PWM time using the gear ratio
+                                                                            //and add it to the current rotation to create a new rotation
+  Serial.println(newRotation);
+  return moveToTargetMicroseconds(newRotation);             // Move to that new rotation
+} 
+
+bool StandardServo::moveAbsolute(float degree){
+  return moveToTargetMicroseconds(mapf((degree / m_outputGearRatio),
+                                        0, 
+                                        m_degreeRange, 
+                                        m_minPWMTime, 
+                                        m_maxPWMTime));     //Remapf the input degrees to servo PWM time using the gear ratio
+}
+
+float StandardServo::getGearRatio(){
+  return m_outputGearRatio;
+}
+
+float StandardServo::getCurrentRotation(){
+  return mapf(m_currentRotation,
+            m_minPWMTime, 
+            m_maxPWMTime, 
+            0, 
+            m_degreeRange * m_outputGearRatio);             //Remapf the PWM timing of the servo to degrees on rotation on the robot
+}
+            
--- INMOOV-Head-Control/StandardServo.h
+++ INMOOV-Head-Control/StandardServo.h
@@ -4,25 +4,85 @@
 #include <Arduino.h>
 #include <Servo.h>
 
+/*
+    Repersents one servo of the robot
+*/
 class StandardServo {
     public:
+        /*
+            Constructor only specifying pin number
+        */
         StandardServo(byte pin);
-        StandardServo(byte pin, float PWMPerDegree, int minRotation, int maxRotation ,bool reverseDirection, int defaultRotation);
+
+        /*
+            Constructor specifying all attributes
+        */
+        StandardServo(byte pin, 
+                      int minPWMTime,           //Min PWM time of the servo
+                      int maxPWMTime,           //Max PWM time of the servo
+                      int degreeRange,          //The range of the servo in degrees(starting at 0)
+                      float outputGearRatio,    //Gear ration of the part that the servo is connected to(mathamatical representation eg. 0.5 = 2/1)
+                      bool reverseDirection,    //Is the rotation of the part different to that of the servo?
+                      int minRotation,          //Min rotation of the part(in degrees)
+                      int maxRotation,          //Max rotation of the part(in degrees) 
+                      int startingRotation);    //The rotation of the part to set when the servo is started(in degrees)
+        
+        /*
+            Modifed arduino standard map function that uses floats
+            Remaps x in the range in_min to in_max, to the range out_min to out_max
+        */
+        float mapf(float x, float in_min, float in_max, float out_min, float out_max);
+        
+        /*
+            Initializes the servo and sets it to its default rotation
+        */
         void attach();
-        bool moveTowardsTarget(float degreesToMove);
-        bool moveToTarget(int target);
-        float getPWMPerDegree();
+
+        /* 
+            Move the robot a given amount from its current position(in degrees)
+            Will constrain the move to min and max rotation
+            Returns true if the move was in bounds, returns false if the move was constrained
+        */
+        bool moveRelative(float degreesToMove);
+
+        /*
+            Move the robot to a specific location(in degrees)
+            Will constrain the move to min and max rotation
+            Returns true if the move was in bounds, returns false if the move was constrained
+        */
+        bool moveAbsolute(float degree);
+
+        /*
+            Returns the gear ratio
+        */
+        float getGearRatio();
+
+        /*
+            Returns the current position of the robot in degrees
+        */
         float getCurrentRotation();
+
     private:
-        bool moveToTargetMicroseconds(int target);
+        /*
+            Writes the target PWM time to the servo
+            Contrains move to the min/max rotation values
+            Returns false if the move was constrained
+        */
+        bool moveToTargetMicroseconds(float target);
+
         byte m_pin = 1;
+        int m_minPWMTime = 1000;  
+        int m_maxPWMTime = 2000;  
+        int m_degreeRange = 180;  
+        
+        float m_outputGearRatio = 1;    
+        bool m_reverseDirection = 0;    
+        int m_minRotation = 1000;       
+        int m_maxRotation = 2000;       
+        int m_startingRotation = 1500;  
+
+        int m_currentRotation = 1500;  
         Servo m_servo;
-        float m_PWMPerDegree = (1000.0/180.0);
-        int m_minRotation = 1000;
-        int m_maxRotation = 2000;
-        bool m_reverseDirection = 0;
-        int m_defaultRotation = 1500;
-        int m_currentRotation = 1500;
 };
 
 #endif
