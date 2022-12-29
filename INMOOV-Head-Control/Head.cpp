1a7b91c177b4c5c9e3e15432a0fbd3cf47a5be8b
Sebastien Robitaille <sebastien@robitaille.info>
2022-12-29 00:24:12 -0800

    add interrupt and interrupt on serial support

--- INMOOV-Head-Control/Head.cpp
+++ INMOOV-Head-Control/Head.cpp
@@ -7,18 +7,18 @@ Head::Head(StandardServo eyesPanServo,
             StandardServo headYawServo,
             StandardServo headPitchServo,
             StandardServo headRollServo,
-            int servoUpdataRate)                //Number of times per second the servos update
+            int servoUpdataRate)                                                //Number of times per second the servos update
 {   
-    m_allServos[0] = eyesPanServo;
+    m_allServos[0] = eyesPanServo;                                              //Add each servo object to the all servos array
     m_allServos[1] = eyesTiltServo;
     m_allServos[2] = jawServo;
     m_allServos[3] = headYawServo;
     m_allServos[4] = headPitchServo;
     m_allServos[5] = headRollServo;
 
-    m_servoUpdateRate = servoUpdataRate;
+    m_servoUpdateRate = servoUpdataRate;                                        //How many times per second the servos' position is updated
 
-    for(byte i = 0; i < 6; i++){    // Set all current/target rotations to the servos' starting rotation
+    for(byte i = 0; i < 6; i++){                                                // Set all current/target rotations to the servos' starting rotation
         m_currentRotations[i] = m_allServos[i].getCurrentRotation();
         m_targetRotations[i] = m_allServos[i].getCurrentRotation();
     }
@@ -54,32 +54,46 @@ bool Head::rotateToTarget(float duration,
                           bool* interruptFlag = 0,
                           bool interruptOnSerial = false)
 {   
-    long startTime = millis();
-    //if the interrupt flag is at its default state, set its reference to a false bool
-    if(interruptFlag == 0){
+    bool interrupted = false;
+    long startTime = millis();                                                  //Get start time of rotation timer 
+
+    if(interruptFlag == 0){                                                     //if the interrupt flag is at its default state, set its reference to a false bool
         const bool interrupt = false;
         interruptFlag = &interrupt;
     }
-    //Do movement stuff....
-    float updateRate = float(m_servoUpdateRate)/1000.0;//Update rate of the servos in ms^-1
+
+    float updateRate = float(m_servoUpdateRate)/1000.0;                         //Update rate of the servos in ms^-1
+
     float degreesPerStep[6];
-    for(byte i; i < 6; i++){  //For each servo
-        float rotationAmount = m_targetRotations[i] - m_currentRotations[i];  //Get the range between the current rotation and the target rotation
-        degreesPerStep[i] = rotationAmount / (updateRate * duration);  //Get the degrees per step
-        //TODO: Check if the rotation per step is to small for the servo to register
+
+    for(byte i; i < 6; i++){                                                    //For each servo
+        float rotationAmount = m_targetRotations[i] - m_currentRotations[i];    //Get the range between the current rotation and the target rotation
+        degreesPerStep[i] = rotationAmount / (updateRate * duration);           //Get the degrees per step
     }
+
     float delayPerStep = 1 / updateRate;
     int count = 1;
-    while(count <= (updateRate * duration)){ //Loop until count has reached number of steps needed
-        if((millis() - startTime) >  (delayPerStep * count)){ //Timer goes off every delayPerStep
+
+    while(count <= (updateRate * duration)){                                    //Loop until count has reached number of steps needed
+        if(interruptOnSerial == 1 && Serial.available() > 0){                   //Exit loop if there is anything in the serial buffer
+            interrupted = true;
+            break;
+        }
+
+        if(*interruptFlag == true){                                             //Exit the loop if the interruptFlag is high
+           interrupted = true;
+           break; 
+        }
+
+        if((millis() - startTime) >  (delayPerStep * count)){                   //Timer goes off every delayPerStep
             count++;
-            for(byte s = 0; s < 6; s++){  //For each servo update its position by one step
+            for(byte s = 0; s < 6; s++){                                        //For each servo update its position by one step
                 m_currentRotations[s] += degreesPerStep[s];
                 m_allServos[s].moveAbsolute(m_currentRotations[s]);
             }
         }
     }
 
-    return false;
+    return interrupted;                                                         //Return true if the loop was interrupted, otherwise return false
 
 }
--- INMOOV-Head-Control/Head.h
+++ INMOOV-Head-Control/Head.h
@@ -8,17 +8,46 @@ class Head {
             StandardServo headYawServo,
             StandardServo headPitchServo,
             StandardServo headRollServo,
-            int servoUpdataRate);
+            int servoUpdataRate);  //How many times per second the servos' position is updated
+
+        /*
+            Attaches all the servos
+        */
         void attach();
+
+        /*
+            Sets the target positions for the eye servos
+        */
         void setEyeRotationTarget(float pan, float tilt);
+
+        /*
+            Sets the target positions for the jaw servo
+        */
         void setJawRotationTarget(float angle);
+
+        /*
+            Sets the target positions for the head rotation servos
+        */
         void setHeadRotationTarget(float yaw, float pitch, float roll);
+
+        /*
+            Moves all servos to their target rotations over the given duration
+
+            If given the adress of interrupt flag, the rotation will be stopped if the flag is set to true
+
+            If interruptOnSerial is set to true, the rotation will be stopped if there is anything in the serial buffer
+            
+            Returns true if the loop was interrupted, returns false if not
+        */
         bool rotateToTarget(float duration, bool* interruptFlag = 0, bool interruptOnSerial = false);
     private:
-        //Order: [0]eye pan, [1]eye tilt, [2]jaw, [3]head yaw, [4]head pitch, [5]head roll
-        float m_targetRotations[6];
-        float m_currentRotations[6];
-        StandardServo m_allServos[6];
-        int m_servoUpdateRate = 40;
+        /*
+            Order for all arrays : [0]eye pan, [1]eye tilt, [2]jaw, [3]head yaw, [4]head pitch, [5]head roll
+        */
+        float m_targetRotations[6];     //Target rotations for all the servos
+        float m_currentRotations[6];    //Current rotatinos for all the servos
+        StandardServo m_allServos[6];   //All the servo objects
+
+        int m_servoUpdateRate = 40;     
         
 };
