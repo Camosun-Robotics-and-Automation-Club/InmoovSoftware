9be1aeb82c7c4354942c47f8f7fdfe9dfb929155
Kayleb814 <Kayleb_s@hotmail.com>
2023-01-01 12:56:30 -0800

    Movement example comments

--- INMOOV-Head-Control/INMOOV-Head-Control.ino
+++ INMOOV-Head-Control/INMOOV-Head-Control.ino
@@ -1,7 +1,47 @@
-void setup(){
+#include "StandardServo.h"
+#include "Head.h"
 
+StandardServo servo_1(3, 500, 2500, 180, 1, 0, 0, 180, 90);
+StandardServo servo_2(5, 500, 2500, 180, 1, 0, 0, 180, 90);
+StandardServo servo_3(6, 500, 2500, 180, 1, 0, 0, 180, 90);
+StandardServo servo_4(7, 500, 2500, 180, 1, 0, 0, 180, 90);
+StandardServo servo_5(8, 500, 2500, 180, 1, 0, 0, 180, 90);
+StandardServo servo_6(9, 500, 2500, 180, 1, 0, 0, 180, 90);
+
+Head head(servo_1,
+          servo_2,
+          servo_3,
+          servo_4,
+          servo_5,
+          servo_6,
+          40);
+
+int duration = 3000;
+volatile bool interupt1 = 0;
+#define interuptPin 2  // Pin number for the digital interupt, !NOTE! only certain pins can be used as interupts
+
+bool interupt()
+{
+  if (digitalRead(interuptPin))
+  {
+    interupt1 = 0;
+  } else {
+    interupt1 = 1;
+  }
+  return interupt1;
 }
 
-void loop(){
-    
+void setup()
+{
+  head.attach();
+  Serial.begin(9600);
+  randomSeed(analogRead(A0));
+  pinMode(interuptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interuptPin), interupt, CHANGE);
+}
+
+
+void loop()
+{
+while(exampleMovement() == false);
 }
