08588277848b77ff17d2dd851d47d000cb0e72f0
aahuinink <a_huinink@hotmail.ca>
2023-01-24 12:41:00 -0800

    added template inheritance

--- INMOOV-Chatbot/chatbot_server/chatbotGUI/GUI/views.py
+++ INMOOV-Chatbot/chatbot_server/chatbotGUI/GUI/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def gui(request):
-    return render(request, 'gui/gui.html')
+    return render(request, 'gui/gui.html', {'title' : 'INMOOV Chatbot'})
