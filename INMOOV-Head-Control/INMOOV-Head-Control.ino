f4e9a57bbd3e73b753c47adcb982ec0d8513e60c
Sebastien Robitaille <sebastien@robitaille.info>
2023-01-30 20:08:17 -0800

    Comment cleanup

--- INMOOV-Head-Control/INMOOV-Head-Control.ino
+++ INMOOV-Head-Control/INMOOV-Head-Control.ino
@@ -16,9 +16,9 @@ Head head(servo_1, // objects included in class
           servo_4, //        "
           servo_5, //        "
           servo_6, //        "
-          40);     //        update frequency
+          40);     //        servo update frequency(hz)
 
-int duration = 500;           //  Time for how long the movement will take place
+int duration = 500;           //  Time for how long the movement will take place(in milliseconds)
 volatile bool interrupt1 = 0; //  Variable for the state of the interrupt
 #define interruptPin 2        //  Pin number for the digital interrupt, !NOTE! only certain pins can be used as interrupts
 
@@ -44,19 +44,18 @@ void setup()
   Serial.begin(9600);                  //  Initialize serial comunications
   randomSeed(analogRead(A0));          //  Use the analog signal from pin A0 to start a random point from
   pinMode(interruptPin, INPUT_PULLUP); //  Initialize a pin for the interrupt and pull it high
-  /*  Initialize an interrupt pin that throughs an interrupt when it's state changes */
+  /*  Initialize an interrupt pin that triggers an interrupt when it's state changes */
   attachInterrupt(digitalPinToInterrupt(interruptPin), interrupt, CHANGE);
 }
 
+/*
+  Main Loop
+  */
 void loop()
 {
-  /*
-  This is the main loop, it runs the example movement function while it returns a false interrupt state
-  it prints the interrupt state and if the example movement returns a true interrupt it exits the loop and
-  prints interrupt detected and waits 5 seconds before looping back to the top and checking the interrupt
-  state returned by the example movement function
-  */
+  /* Run the example movement routine until it returns true(meaning an interrupt was triggerd during the movement)*/
   while (exampleMovement() == false);
+  /* Place code that need to run after an interrupt here*/
   Serial.println("Interrupt1 Triggered");
   delay(2000);
 }
